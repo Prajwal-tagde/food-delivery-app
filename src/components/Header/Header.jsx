3dd034e462dd1237ec6a931a5d1cb7d273fe8c38
Prajwal Tagde <86796487+Prajwal-tagde@users.noreply.github.com>
2024-03-22 20:50:46 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Favorite Food Here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, beatae. Provident voluptatibus eligendi numquam perspiciatis 
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
